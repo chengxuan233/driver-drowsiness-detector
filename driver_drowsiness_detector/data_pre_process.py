9502ac8d5af6afe63f423a2662b6ffc8c245086e
silver233jpg <60947716+silver233jpg@users.noreply.github.com>
2023-08-26 23:03:50 -0400

    define ImageGenerator class

--- driver_drowsiness_detector/data_pre_process.py
+++ driver_drowsiness_detector/data_pre_process.py
@@ -2,3 +2,24 @@
 This script is used to:
 driver image cleaning, labeling, and converting to model training data
 """
+from keras.preprocessing import image
+
+
+class ImageGenerator(object):
+    """
+    utility generator for iterating images from given directory.
+    """
+
+    def __init__(self, directory: str):
+        """
+        generator constructor.
+        :param directory: image directory to be iterated.
+        """
+        self.directory = directory
+
+    def generator(self) -> image.ImageDataGenerator:
+        """
+        return a generator for the given directory.
+        :return: image generator
+        """
+        pass
