294f1e7b2001a6b0ed23e4fdf2fbd86f60600290
silver233jpg <60947716+silver233jpg@users.noreply.github.com>
2023-08-30 10:09:47 -0400

    define ModelTrain class

--- driver_drowsiness_detector/model_train.py
+++ driver_drowsiness_detector/model_train.py
@@ -2,3 +2,28 @@
 This script is used to:
 train driver drowsiness detector model to classify diver eyes into 'open' and 'close' classes
 """
+
+
+class ModelTrain(object):
+    """
+    this class is used to train model to classify driver drowsiness.
+    """
+
+    def __init__(self, directory: str):
+        """
+        class constructor.
+        :param directory: image directory to be iterated.
+        """
+        self.directory = directory
+
+    def train(self):
+        """
+        train the driver drowsiness classifier model using the training dataset
+        """
+        pass
+
+    def save(self, path: str):
+        """
+        save the trained model in given path
+        """
+        pass
